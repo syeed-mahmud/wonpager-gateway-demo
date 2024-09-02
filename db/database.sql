4fcf0614072578b0955b01947f9e1b129ca6d2d5
syeed-mahmud <mluban100@gmail.com>
2024-09-02 13:31:33 -0400

    added dashboard landing page
    added dashboard landing page

--- db/database.sql
+++ db/database.sql
@@ -1,9 +1,26 @@
-CREATE DATABASE wonpager;
+CREATE DATABASE  wp_modules;
 
-USE wonpager;
+USE  wp_modules;
 
-CREATE TABLE userlist (
+CREATE TABLE users (
+    user_id BIGINT(20) NOT NULL AUTO_INCREMENT,
     user_name VARCHAR(20) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    module VARCHAR(200) NOT NULL
+    lic_code VARCHAR(200) NOT NULL,
+        PRIMARY KEY (user_id)
+);
+
+CREATE TABLE modules (
+    module_id BIGINT(20) NOT NULL AUTO_INCREMENT,
+    module  VARCHAR(255) NOT NULL,
+    lic_code VARCHAR(10) NOT NULL,
+        PRIMARY KEY (module_id)
+);
+ 
+CREATE TABLE feature_flags (
+    feature_id BIGINT(20) NOT NULL AUTO_INCREMENT,
+    feature VARCHAR(20) NOT NULL,
+    module_id VARCHAR(255) NOT NULL,
+    allow_access boolean Default false,
+        PRIMARY KEY (feature_id)
 );
